49f8468b61934fdf7439e83cb1ccd596de29309e
Erik Sandberg <erik@sandberg.net>
2015-08-18 11:00:05 -0700

    Updated ruby version and some cleanup

--- setup.sh
+++ setup.sh
@@ -1,91 +1,92 @@
 echo "Starting installation process..."
-echo "Install packages? (y/n)"
+echo "Install packages? (y/N)"
 read installPackages
-echo "Download & Install Qt 4.7.4? (y/n)"
+echo "Download & Install Qt 4.7.4? (y/N)"
 read installQt
-echo "Download & Install Ruby 1.9.3? (y/n)"
+echo "Download & Install Ruby? (y/N)"
 read installRuby
-echo "Download & Install Git? (y/n)"
+if [[ "$installRuby" == "y" ]]; then
+    DEFAULT_RUBY_VERSION=2.1.5
+    echo "Ruby Version? (${DEFAULT_RUBY_VERSION})"
+    read rubyVersion
+    rubyVersion=${rubyVersion:-${DEFAULT_RUBY_VERSION}}
+fi
+echo "Setup Git? (y/N)"
 read installGit
-echo "Clone TechUSB Repo? (y/n)"
-read cloneRepo
 
 if [[ "$installPackages" == "y" ]]; then
-	echo "Updating..."
-	sudo apt-get -y update
-	echo "Installing necessary packages..."
-	sudo apt-get -y install curl git-core python-software-properties build-essential openssl libssl-dev python g++ make checkinstall
-	sudo apt-get -y install postgresql libpq-dev xclip libxext-dev live-build
-	sudo mkdir ~/src && cd $_
-	sudo wget -N http://nodejs.org/dist/node-latest.tar.gz
-	sudo tar xzvf node-latest.tar.gz && cd node-v*
-	sudo ./configure
-	sudo checkinstall
-	sudo dpkg -i node_*
+    echo "Updating..."
+    sudo apt-get -y update
+    echo "Installing necessary packages..."
+    sudo apt-get -y install curl git-core python-software-properties build-essential openssl libssl-dev python g++ make checkinstall
+    sudo apt-get -y install postgresql libpq-dev xclip libxslt-dev libxml2-dev nodejs nginx imagemagick libmagickcore-dev libmagickwand-dev
+    sudo mkdir ~/src && cd $_
+    sudo wget -N http://nodejs.org/dist/node-latest.tar.gz
+    sudo tar xzvf node-latest.tar.gz && cd node-v*
+    sudo ./configure
+    sudo checkinstall
+    sudo dpkg -i node_*
 fi
 
 
 if [[ "$installQt" == "y" ]]; then
-	cd /tmp
-	sudo wget -N http://download.qt-project.org/archive/qt/4.7/qt-everywhere-opensource-src-4.7.4.tar.gz
-	sudo tar xzvf qt-everywhere-opensource-src-4.7.4.tar.gz
-	cd qt-everywhere-opensource-src-4.7.4
-	sudo ./configure
-	echo "How many CPU cores does this computer have? (Choose: 1,2,4)"
-	sudo ln -s /usr/lib/libXrender.so.1.3.0 /usr/lib/libXrender.so
-	read cores
-	if [[ "$cores" == "1" ]]; then
-		make
-	elif [[ "$cores" == "2" ]]; then
-		make -j4
-	elif [[ "$cores" == "4" ]]; then 
-		make -j8
-	else
-		make
-	fi
-	sudo make install
-	echo "PATH=/usr/local/Trolltech/Qt-4.7.4/bin:$PATH" >> ~/.bashrc
-	echo "export PATH" >> ~/.bashrc	
+    cd /tmp
+    sudo wget -N http://download.qt-project.org/archive/qt/4.7/qt-everywhere-opensource-src-4.7.4.tar.gz
+    sudo tar xzvf qt-everywhere-opensource-src-4.7.4.tar.gz
+    cd qt-everywhere-opensource-src-4.7.4
+    sudo ./configure
+    echo "How many CPU cores does this computer have? (Choose: 1,2,4)"
+    sudo ln -s /usr/lib/libXrender.so.1.3.0 /usr/lib/libXrender.so
+    read cores
+    if [[ "$cores" == "1" ]]; then
+        make
+    elif [[ "$cores" == "2" ]]; then
+        make -j4
+    elif [[ "$cores" == "4" ]]; then 
+        make -j8
+    else
+        make
+    fi
+    sudo make install
+    echo "PATH=/usr/local/Trolltech/Qt-4.7.4/bin:$PATH" >> ~/.bashrc
+    echo "export PATH" >> ~/.bashrc    
 fi
 
 if [[ "$installRuby" == "y" ]]; then
-	echo "Downloading and Installing Ruby 1.8.7-p371 via Rbenv..."
-	curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
-	echo "Copy & Paste the lines above as stated into the bashrc file, then press enter"
-	read enter
-	. ~/.bashrc
-	rbenv install 1.9.3-p125
-	rbenv global 1.9.3-p125
-	ruby -v
-	gem install bundler --no-ri --no-rdoc
-	gem install rake
-	gem install mustache
-	rbenv rehash
-	cd ~/.ssh
-	bundle -v
-	sudo -u postgres psql
+    echo "Downloading and Installing Ruby $rubyVersion via Rbenv..."
+    curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+    echo "Copy & Paste the lines above as stated into the bashrc file, then press enter"
+    read enter
+    . ~/.bashrc
+    rbenv install $rubyVersion
+    rbenv global $rubyVersion
+    ruby -v
+    gem install bundler --no-ri --no-rdoc
+    gem install rake
+    rbenv rehash
+    cd ~/.ssh
+    bundle -v
+    echo "Please make a postgres user and db. Use the following commands:"
+    echo "create user rt password 'password123';"
+    echo "create database rt_production owner rt;"
+    echo "\quit"
+    sudo -u postgres psql
 fi
 
 
 if [[ "$installGit" == "y" ]]; then
-	echo "Type your email address, followed by [ENTER]:"
-	read address
-	ssh-keygen -t rsa -C "$address"
-	ssh-add
-	xclip -sel clip < ~/.ssh/id_rsa.pub
-	echo "What is your name?"
-	read username
-	git config --global user.email "$address"
-	git config --global user.name "$username"
-	echo "You're all set."
-	echo "Copied ssh key to clipboard, please paste into your github account on github.com, then press enter."
-	read enter
-fi
-
-if [[ "$cloneRepo" == "y" ]]; then
-	mkdir -p ~/Repositories
-	cd ~/Repositories
-	git clone git@github.com:thai510/Tech-USB.git
+    echo "Type your email address, followed by [ENTER]:"
+    read address
+    ssh-keygen -t rsa -C "$address"
+    ssh-add
+    xclip -sel clip < ~/.ssh/id_rsa.pub
+    echo "What is your name?"
+    read username
+    git config --global user.email "$address"
+    git config --global user.name "$username"
+    echo "You're all set."
+    echo "Copied ssh key to clipboard, please paste into your github account on github.com, then press enter."
+    read enter
 fi
 
 echo "ALL DONE!"
